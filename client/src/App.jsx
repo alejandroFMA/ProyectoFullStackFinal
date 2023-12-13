9cb701dbf37bfcbf2f084fe36ed55dccfec5acdc
Alex Marquez <alexfmarquez@gmail.com>
2023-12-13 14:29:23 +0100

    add/basic components

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Main from './components/Main'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <><Header/>
+      <Main/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Main/Home/Form/Form.jsx
@@ -0,0 +1,12 @@
+
+const Form = () => {
+  return <form>
+    <label htmlFor="nombre"></label>
+    <input type="text"></input>
+    <button type="submit">Buscar</button>
+
+
+    </form>;
+};
+
+export default Form;
