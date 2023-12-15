10dce36294184a9dfaf4615a7c13c2f38156a778
Alex Marquez <alexfmarquez@gmail.com>
2023-12-15 18:51:18 +0100

    change controller

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -2,14 +2,14 @@ const Comment = require("../models/commentsModel");
 
 const getComment = async (req, res) => {
   try {
-      const commentId = req.params.id; 
-      const comment = await Comment.getComment(commentId); 
-      if (!comment) {
-          return res.status(404).json({ message: 'Comentario no encontrado' });
-      }
-      res.json(comment);
+    const commentId = req.params.id;
+    const comment = await Comment.getComment(commentId);
+    if (!comment) {
+      return res.status(404).json({ message: "Comentario no encontrado" });
+    }
+    res.status(200).json(comment);
   } catch (error) {
-      res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
@@ -29,58 +29,80 @@ const getComment = async (req, res) => {
 //     }
 // };
 
-const getAllComments = async (req, res) => {
-    try {
-        const comment = await Comment.getAllComments(); 
-        if  (comment.length === 0) { 
-            return res.status(404).json({ message: 'Comentario no encontrado' });
-        }
-        res.json (comment); 
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-
-const createComment= async (req, res) => {
+const getCommentsbyRestaurantID = async (req, res) => {
   try {
-      const commentData = req.body; 
-      const comment = await Comment.createReservation(commentData);
-      res.status(201).json (comment);
+    const restaurantId = req.query.params;
+    if (!restaurantId) {
+      return res.status(400).json({ message: "ID de restaurante requerido" });
+    }
+
+    const comments = await Comment.getCommentsbyRestaurantID(restaurantId);
+
+    if (!comments) {
+      return res.status(404).json({ message: "Comentarios no encontrados" });
+    }
+
+    res.status(200).json(comments);
   } catch (error) {
-      res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
+const getAllComments = async (req, res) => {
+  try {
+    const comment = await Comment.getAllComments();
+    if (comment.length === 0) {
+      return res.status(404).json({ message: "Comentario no encontrado" });
+    }
+    res.status(200).json(comment);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createComment = async (req, res) => {
+  try {
+    const commentData = req.body;
+    const comment = await Comment.createComment(commentData);
+    res.status(201).json(comment);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const updateComment = async (req, res) => {
   try {
-      const commentId = req.params.id; 
-      const updateData = req.body; 
-      const comment = await Comment.updateReservation(commentId, updateData)
-      if ( comment) {
-          return res.status(404).json({ message: 'Comentario no encontrado' });
-      }
-      res.json (comment);
+    const commentId = req.params.id;
+    const updateData = req.body;
+    const comment = await Comment.updateReservation(commentId, updateData);
+    if (comment) {
+      return res.status(404).json({ message: "Comentario no encontrado" });
+    }
+    res.json(comment);
   } catch (error) {
-      res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
-
-
 
 const deleteComment = async (req, res) => {
-    try {
-        const commentId = req.params.id;
-        const comment = await Reservation.deleteReservation(commentId);
-        if ( comment) {
-            return res.status(404).json({ message: 'Comentario no encontrado' });
-        }
-  
-        res.status(200).json({ message: 'Comentario eliminado' });
+  try {
+    const commentId = req.params.id;
+    const comment = await Reservation.deleteReservation(commentId);
+    if (comment) {
+      return res.status(404).json({ message: "Comentario no encontrado" });
+    }
+
+    res.status(200).json({ message: "Comentario eliminado" });
   } catch (error) {
-      res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
-module.exports = {getComment, getAllComments, createComment, updateComment, deleteComment};
+module.exports = {
+  getComment,
+  getAllComments,
+  getCommentsbyRestaurantID,
+  createComment,
+  updateComment,
+  deleteComment,
+};
