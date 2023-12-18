208de8f03a2a0811236c1d669c115e255715bf27
alejandroFMA <alexfmarquez@gmail.com>
2023-12-18 19:28:29 +0100

    add/footer

--- index.js
+++ index.js
@@ -72,6 +72,8 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'dist','index.html')));
 }
 
+
+
 app.listen(port, () => {
   console.log(`Listening on port: http://localhost:${port}`)
  
