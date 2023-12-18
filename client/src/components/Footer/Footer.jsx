ddf45e9a86a1dcfb76ea6d52356815aa9ad294d4
alejandroFMA <alexfmarquez@gmail.com>
2023-12-18 19:58:35 +0100

    change index

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <footer>
+  <p>Desarrollado por </p>
+  <a href="https://github.com/alejandroFMA/ProyectoFullStackFinal"> Alex Marquez</a>
+  </footer>;
 };
 
 export default Footer;
